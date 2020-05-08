a42986b227735de6cf7b923a6a2dd066b23a8e2f
normRoa <normanroam@gmail.com>
2020-05-08 14:57:44 -0700

    Fetching Data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,34 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { Component } from 'react'
+import fetch from 'isomorphic-unfetch'
+export default class About extends Component {
 
-export default () => (
-  <Layout title="About">
-    <p>JavaScript programmer</p>
-    <img src="/static/javascript-logo.png" alt="Javascript" height="200px" />
-  </Layout>
-)
+  static async getInitialProps() {
+    const res = await fetch('https://api.github.com/users/nroa')
+    const data = await res.json()
+
+    return { user: data };
+  }
+
+  // componentDidMount() {
+  //   fetch('https://api.github.com/users/nroa')
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     this.setState({
+  //       user: data
+  //     });
+  //   })
+  // }
+
+  render() {
+    const { user } = this.props;
+
+    return(
+      <Layout title="About">
+        <p>{user.name}</p>
+        <img src={user.avatar_url} alt="Norm" height="200px" />
+      </Layout>
+    )
+  }
+}

51e758983d8b53fecc56c8841a60f52a5fdf9e14
normRoa <normanroam@gmail.com>
2020-05-08 12:34:37 -0700

    Components, and Styled JSX

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,9 @@
 import Link from 'next/link'
+import Layout from '../components/Layout'
 
 export default () => (
-  <div>
-    <h1>About</h1>
-    <Link href="/">
-      <a>Go to Home</a>
-    </Link>
+  <Layout title="About">
     <p>JavaScript programmer</p>
     <img src="/static/javascript-logo.png" alt="Javascript" height="200px" />
-  </div>
+  </Layout>
 )

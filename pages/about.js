10547c72ac5bdfa684b351825f8d51a0ae5a622c
normRoa <normanroam@gmail.com>
2020-05-08 15:14:29 -0700

    Custom Error Page

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,16 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 import { Component } from 'react'
 import fetch from 'isomorphic-unfetch'
+import Error from './_error';
+
 export default class About extends Component {
 
   static async getInitialProps() {
-    const res = await fetch('https://api.github.com/users/nroa')
+    const res = await fetch('https://api.github.com/users/nrofdsfsdfa')
+    const statusCode = res.status > 200 ? res.status : false
     const data = await res.json()
 
-    return { user: data };
+    return { user: data, statusCode };
   }
 
   // componentDidMount() {
@@ -22,7 +24,11 @@ export default class About extends Component {
   // }
 
   render() {
-    const { user } = this.props;
+    const { user, statusCode } = this.props;
+
+    if (statusCode) {
+      return <Error statusCode={statusCode} />
+    }
 
     return(
       <Layout title="About">
